e58b5962cbd2ba04b3afe565bda100db408bdbaf
meno.m <meno.m@opst.co.jp>
2018-02-20 17:19:28 +0900

    追加：Azure mongoDBを使用するように修正

--- app/app.js
+++ app/app.js
@@ -1,11 +1,26 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+const env = require('./env/environment');
 
 // DB設定
 var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/ExpressAPI');
+
+// ローカルDBのURI
+//const mongoUri = 'mongodb://localhost:27017/ExpressAPI';
+
+// Azure公式チュートリアルに載っていたURI（mongoose v5.0.0以上だと  UnhandledPromiseRejectionWarning が発生して接続できない
+//const mongoUri = `mongodb://${env.accountName}:${env.key}@${env.accountName}.documents.azure.com:${env.port}/?ssl=true&replicaSet=globaldb`;
+const mongoUri = `mongodb://${env.accountName}.documents.azure.com:${env.port}/?ssl=true&replicaSet=globaldb`;
+
+mongoose.connect(mongoUri, {
+    auth: {
+        user: env.accountName,
+        password: env.key
+    }
+});
+
 mongoose.connection.on('error', function (err) {
     console.error('MongoDB connection error: ' + err);
     process.exit(-1);
