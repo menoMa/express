b88a9481d5bc236f0e8e5cafb1ecf68e6c27f29d
meno.m <meno.m@opst.co.jp>
2018-02-20 17:24:33 +0900

    Revert "追加：Azure mongoDBを使用するように修正"
    This reverts commit e58b5962cbd2ba04b3afe565bda100db408bdbaf.

--- app/env/environment.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    accountName: 'menom-mongodb',
-    databaseName: 'ExpressApi',
-    key: 'lpERzKk5sV9biG8ao5eWdfTVjyEdbwm3NisSAwwTtAhC2KRtHXkLlpDXJPtayT1A1v0cqK7FJcCOdqdTTSV25Q==',
-    port: 10255
-};
